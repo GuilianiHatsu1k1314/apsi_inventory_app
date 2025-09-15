a510ba3da89e2d54e11b432ba0f8ecfa3f6ea3a6
Dean <deanlt.santos@gmail.com>
2025-09-15 09:27:20 +0800

    migrate to ReactDOM.createRoot with RouterProvider setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([]);
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
