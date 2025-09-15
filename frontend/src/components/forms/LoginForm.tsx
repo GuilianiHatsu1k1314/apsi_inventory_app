fa06c31996614e445b92673098c02240f3d08181
Dean <deanlt.santos@gmail.com>
2025-09-15 15:29:53 +0800

    add style to login page (background design will be added later)

--- frontend/src/components/forms/LoginForm.tsx
+++ frontend/src/components/forms/LoginForm.tsx
@@ -1,15 +1,25 @@
 export default function LoginForm() {
 	return(
-		<form>
+		<form className="flex flex-col w-sm p-14 pt-0 gap-8">
 			<input 
 				type="text"
 				placeholder="Username"
+				className="border-3 p-2 font-semibold 
+									 placeholder-[var(--dali-purple)]
+									 border-[var(--dali-purple)] rounded-[16px]"
 			/>
 			<input 
 				type="password"
 				placeholder="Password"
+				className="border-3 p-2 font-semibold
+								   placeholder-[var(--dali-purple)]
+									 border-[var(--dali-purple)] rounded-[16px]"
 			/>
-			<button>
+			<button 
+				type="submit"
+				className="w-34 px-4 py-3 self-center bg-[var(--dali-purple)] text-white font-semibold rounded-[17px]
+									 hover:bg-violet-600"
+			>
 				Submit
 			</button>
 		</form>
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,9 @@
 import LoginForm from "../components/forms/LoginForm";
-
+import logo from '../assets/logo.png'
 export default function LoginPage(){
 	return (
-		<main>
+		<main className="h-screen flex flex-col justify-center items-center">
+			<img src={logo} className="h-82"/>
 			<LoginForm />
 		</main>
 	);
