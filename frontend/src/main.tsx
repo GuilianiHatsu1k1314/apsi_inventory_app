e86633fdf98fd1482b5a5f7a93b843eb733e3420
Dean <deanlt.santos@gmail.com>
2025-09-15 09:40:15 +0800

    add route to login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
+import LoginPage from './pages/LoginPage';
 
-const router = createBrowserRouter([]);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage />,
+    errorElement: <div>404 not found</div>
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router}/>
