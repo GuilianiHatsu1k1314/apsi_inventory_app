e37f86609a3ab976f8e84485ef4661a0be951cdb
Dean <deanlt.santos@gmail.com>
2025-09-15 12:08:03 +0800

    setup route provider and set Loginpage as index route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <RouterProvider router={router} />
   </StrictMode>,
 )
