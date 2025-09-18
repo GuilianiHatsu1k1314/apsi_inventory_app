3204de6173f48c53f4f85157c20d66807a7376ef
Dean <deanlt.santos@gmail.com>
2025-09-18 12:13:09 +0800

    add logo to header and change header color

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,8 @@
+import logo from '../assets/logo.png'
 export const Header = () => {
 	return(
-		<header className="w-full flex bg-[var(--dali-purple)]">
-			<div className="h-[60px]">Logo</div>
+		<header className="w-full flex bg-purple-300">
+			<img src={logo} className='h-20'/>
 			<h1>Admin</h1>
 		</header>
 	);
