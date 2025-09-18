93af027c59d9ab6dc6c82175bb6fcd4848b7d57f
Dean <deanlt.santos@gmail.com>
2025-09-18 09:46:39 +0800

    add link to dashboard tab

--- frontend/src/components/SideBar/SideBarNav.tsx
+++ frontend/src/components/SideBar/SideBarNav.tsx
@@ -1,9 +1,10 @@
+import {Link} from 'react-router-dom';
 export const NavBar = () => {
 	return(
 		<aside className="h-full w-3xs bg-gray-200">
 			<nav className="pt-5">
 				<ul>
-					<li className="hover:bg-[var(--dali-purple)] hover:text-white">Dashboard</li>
+					<li className="hover:bg-[var(--dali-purple)] hover:text-white"><Link to="/app">Dashboard</Link></li>
 					<li className="hover:bg-[var(--dali-purple)] hover:text-white">Customer</li>
 					<li className="hover:bg-[var(--dali-purple)] hover:text-white">Distributor</li>
 					<li className="hover:bg-[var(--dali-purple)] hover:text-white">Warehouse Module</li>
