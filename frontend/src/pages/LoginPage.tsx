73d4afbc90bcb9c1ddf9139e7119696ba3416647
Dean <deanlt.santos@gmail.com>
2025-09-18 15:17:03 +0800

    fixed formatting

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,19 +1,19 @@
 import LoginForm from "../components/forms/LoginForm";
 import { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from '../assets/logo.png'
 
 export const LoginPage = () => {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  return (
-    <main className="h-screen flex flex-col justify-center items-center">
-      <img src={logo} className="h-82" />
-      <LoginForm
-        username={username}
-        setUsername={setUsername}
-        password={password}
-        setPassword={setPassword}
-      />
-    </main>
-  );
-};
+	const[username, setUsername] = useState<string>("");
+	const[password, setPassword] = useState<string>("");
+	return (
+		<main className="h-screen flex flex-col justify-center items-center">
+			<img src={logo} className="h-82"/>
+			<LoginForm
+				username={username}
+				setUsername={setUsername}
+				password={password}
+				setPassword={setPassword}	
+			/>
+		</main>
+	);
+}
