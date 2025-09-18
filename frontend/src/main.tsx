1826e4d62d7987532120240451397fd855a85ae8
Dean <deanlt.santos@gmail.com>
2025-09-19 04:36:28 +0800

    edit imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,11 +6,12 @@ import { LoginPage } from "./pages/LoginPage";
 import { InventoryAppPage } from "./pages/InventoryAppPage";
 import { Dashboard } from "./pages/Admin/Dashboard";
 import { Customer } from "./pages/Admin/Customer";
-import WarehouseModule from "./pages/Admin/Warehouse";
+import {WarehousePage} from "./pages/Admin/Warehouse";
 import { Distributor } from "./pages/Admin/Distributor";
 import { AccountingPage } from "./pages/Admin/Accounting";
 import { RegisterPage } from "./pages/RegisterPage";
 import { CSRPage } from "./pages/Admin/CustomerService";
+import { TeamLeaderPage } from "./pages/Admin/TeamLead";
 
 const router = createBrowserRouter([
   {
@@ -35,7 +36,7 @@ const router = createBrowserRouter([
       },
       {
         path: "warehouse",
-        element: <WarehouseModule />
+        element: <WarehousePage />
       },
       {
         path: "distributor",
@@ -48,6 +49,10 @@ const router = createBrowserRouter([
       {
         path: "customer-service",
         element: <CSRPage />
+      },
+            {
+        path: "team-lead",
+        element: <TeamLeaderPage />
       },
     ],
   },
