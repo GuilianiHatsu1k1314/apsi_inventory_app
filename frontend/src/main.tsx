ff9705c2c8c302d8aa219ce12242867f250e1bbc
Dean <deanlt.santos@gmail.com>
2025-09-18 09:45:44 +0800

    add main route with children routes (for the sidebar navigation)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,30 @@
-import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import {LoginPage} from './pages/LoginPage';
-import {InventoryAppPage} from './pages/InventoryAppPage';
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { InventoryAppPage } from "./pages/InventoryAppPage";
+import { Dashboard } from "./pages/Dashboard";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <LoginPage />
+    path: "/",
+    element: <LoginPage />,
   },
-  {
-    path: '/app',
-    element: <InventoryAppPage />
-  }
+  { 
+    path: "app",
+    element: <InventoryAppPage />,
+    children: [
+      { 
+        index: true,
+        element: <Dashboard /> 
+      }
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
