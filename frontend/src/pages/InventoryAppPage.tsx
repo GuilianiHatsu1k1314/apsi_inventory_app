1e3950354af8a62c11193e383334b8f3e63a1437
Dean <deanlt.santos@gmail.com>
2025-09-18 09:46:23 +0800

    fix layout to render Outlet properly

--- frontend/src/pages/InventoryAppPage.tsx
+++ frontend/src/pages/InventoryAppPage.tsx
@@ -1,11 +1,17 @@
+import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
 import { NavBar } from "../components/SideBar/SideBarNav";
 
 export const InventoryAppPage = () => {
-  return(
-  	<main className="h-screen overflow-hidden">
-			<Header />
-			<NavBar />
-		</main> 
-	);
-}
+  return (
+    <div className="h-screen flex flex-col overflow-hidden">
+      <Header />
+      <div className="h-full flex flex-1 overflow-hidden">
+        <NavBar />
+        <main className="flex-1 overflow-y-auto p-4 bg-gray-100">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
