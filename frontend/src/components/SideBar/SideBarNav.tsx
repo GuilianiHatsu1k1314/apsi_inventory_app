72fd5d23daa12e9147d21e679ca96c0fc4d58122
Dean <deanlt.santos@gmail.com>
2025-09-18 08:47:04 +0800

    create basic html structure for side bar

---
+++ frontend/src/components/SideBar/SideBarNav.tsx
@@ -0,0 +1,14 @@
+export const NavBar = () => {
+	return(
+		<aside>
+			<nav>
+				<ul>
+					<li>Dashboard</li>
+					<li>Customer</li>
+					<li>Distributor</li>
+					<li>Warehouse Module</li>
+				</ul>
+			</nav>
+		</aside>
+	);
+}
