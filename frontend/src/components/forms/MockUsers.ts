ff0e150cd468f1b676403e39fa968ef2ef6b56c9
Dean <deanlt.santos@gmail.com>
2025-09-18 15:15:06 +0800

    create mock users for testing login

---
+++ frontend/src/components/forms/MockUsers.ts
@@ -0,0 +1,8 @@
+export const mockUsers = [
+  { username: "admin", password: "123", role: "Admin" },
+  { username: "csr", password: "123", role: "CSR" },
+  { username: "warehouse", password: "123", role: "Warehouse" },
+  { username: "customer", password: "123", role: "Customer" },
+  { username: "accounting", password: "123", role: "Accounting" },
+  { username: "leader", password: "123", role: "TeamLeader" },
+];
