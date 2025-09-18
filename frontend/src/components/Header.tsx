600ccf9540521679b699920c7f4e7600498aec0c
Dean <deanlt.santos@gmail.com>
2025-09-18 09:10:25 +0800

    add basic styling (add assets and polished later)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 export const Header = () => {
 	return(
-		<header>
+		<header className="w-full flex bg-[var(--dali-purple)]">
+			<div className="h-[60px]">Logo</div>
 			<h1>Admin</h1>
 		</header>
 	);
