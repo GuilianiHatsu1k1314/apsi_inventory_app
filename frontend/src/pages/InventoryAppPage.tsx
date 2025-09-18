32bdb12270fc8f383d59bb1db215d57e24114767
Dean <deanlt.santos@gmail.com>
2025-09-18 08:58:46 +0800

    add sidebar to main app

--- frontend/src/pages/InventoryAppPage.tsx
+++ frontend/src/pages/InventoryAppPage.tsx
@@ -1,7 +1,9 @@
+import { NavBar } from "../components/SideBar/SideBarNav";
+
 export const InventoryAppPage = () => {
   return(
-  	<>
-	 
-		</>
+  	<main className="h-screen">
+			<NavBar />
+		</main> 
 	);
 }
