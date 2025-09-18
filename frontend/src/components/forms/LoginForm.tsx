b05139c4610a71d7897f7a909281e84d0dab3af3
Kurt Sardes <18kurt.sardes@gmail.com>
2025-09-18 23:13:11 +0800

    Added NEW PROPERTIES

--- frontend/src/components/forms/LoginForm.tsx
+++ frontend/src/components/forms/LoginForm.tsx
@@ -1,61 +1,62 @@
-import { useNavigate } from "react-router-dom";
-import { mockUsers } from "./MockUsers";
 interface Props {
-	username: string;
-	setUsername: React.Dispatch<React.SetStateAction<string>>;
-	password: string;
-	setPassword:React.Dispatch<React.SetStateAction<string>>;
+  username: string;
+  setUsername: React.Dispatch<React.SetStateAction<string>>;
+  password: string;
+  setPassword: React.Dispatch<React.SetStateAction<string>>;
+  onSubmit: () => void;
+  loading: boolean;
+  error?: string | null;
 }
-export default function LoginForm({username, setUsername, password, setPassword}: Props) {
-	const navigate = useNavigate();
-	const handleClick = (e: React.FormEvent) => {
-		
-    e.preventDefault(); 
-    const user = mockUsers.find(
-      (u) => u.username === username && u.password === password
-    );
 
-    if (!user) {
-      alert("Invalid username or password");
-      return;
-    }
-	switch (user.role) {
-      case "Admin":
-        navigate('/app');
-        break;
-	}
-	}
-	return(
-		<form
-			onSubmit={handleClick} 
-			className="flex flex-col w-sm p-14 pt-0 gap-8">
-			<input 
-				type="text"
-				placeholder="Username"
-				required
-				onChange={(e) => setUsername(e.target.value)}
-				className="border-3 p-2 font-semibold 
-									 placeholder-[var(--dali-purple)]
-									 border-[var(--dali-purple)] rounded-[16px]
-									 focus:border-violet-600 focus:outline-none"
-			/>
-			<input 
-				type="password"
-				placeholder="Password"
-				required
-				onChange={(e) => setPassword(e.target.value)}
-				className="border-3 p-2 font-semibold
-								   placeholder-[var(--dali-purple)]
-									 border-[var(--dali-purple)] rounded-[16px]
-									 focus:border-violet-600 focus:outline-none"
-			/>
-			<button 
-				type="submit"
-				className="w-34 px-4 py-3 self-center bg-[var(--dali-purple)] text-white font-semibold rounded-[17px]
-									 hover:bg-violet-600"
-			>
-				Submit
-			</button>
-		</form>
-	);
+export default function LoginForm({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  onSubmit,
+  loading,
+  error,
+}: Props) {
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit();
+      }}
+      className="flex flex-col w-sm p-14 pt-0 gap-8"
+    >
+      <input
+        type="email"
+        placeholder="Email"
+        required
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="border-3 p-2 font-semibold 
+                   placeholder-[var(--dali-purple)]
+                   border-[var(--dali-purple)] rounded-[16px]
+                   focus:border-violet-600 focus:outline-none"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="border-3 p-2 font-semibold
+                   placeholder-[var(--dali-purple)]
+                   border-[var(--dali-purple)] rounded-[16px]
+                   focus:border-violet-600 focus:outline-none"
+      />
+      <button
+        type="submit"
+        disabled={loading}
+        className="w-34 px-4 py-3 self-center bg-[var(--dali-purple)] text-white font-semibold rounded-[17px]
+                   hover:bg-violet-600"
+      >
+        {loading ? "Logging in..." : "Submit"}
+      </button>
+
+      {error && <p className="text-red-500">{error}</p>}
+    </form>
+  );
 }
