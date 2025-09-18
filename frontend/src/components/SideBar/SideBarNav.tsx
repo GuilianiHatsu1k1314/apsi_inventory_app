66dbc00a3acd069eebada8f14efb2bc5c15a085b
Dean <deanlt.santos@gmail.com>
2025-09-18 10:40:24 +0800

    Link customer tab

--- frontend/src/components/SideBar/SideBarNav.tsx
+++ frontend/src/components/SideBar/SideBarNav.tsx
@@ -4,8 +4,8 @@ export const NavBar = () => {
 		<aside className="h-full w-3xs bg-gray-200">
 			<nav className="pt-5">
 				<ul>
-					<li className="hover:bg-[var(--dali-purple)] hover:text-white"><Link to="/app">Dashboard</Link></li>
-					<li className="hover:bg-[var(--dali-purple)] hover:text-white">Customer</li>
+					<li className="hover:bg-[var(--dali-purple)] hover:text-white"><Link to="/app" className='block'>Dashboard</Link></li>
+					<li className="hover:bg-[var(--dali-purple)] hover:text-white"><Link to="/app/customer">Customer</Link></li>
 					<li className="hover:bg-[var(--dali-purple)] hover:text-white">Distributor</li>
 					<li className="hover:bg-[var(--dali-purple)] hover:text-white">Warehouse Module</li>
 					<li className="mt-16 hover:bg-[var(--dali-purple)] hover:text-white">Reports</li>
